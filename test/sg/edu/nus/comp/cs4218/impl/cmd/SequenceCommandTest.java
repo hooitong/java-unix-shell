7e359df4995c3b7aee20480b85c0f9a67611d7e7
Hooi Tong <hooitongx@gmail.com>
2016-02-09 16:20:40 +0800

    Added test stubs for `CallCommand` and `SequenceCommand`

---
+++ test/sg/edu/nus/comp/cs4218/impl/cmd/SequenceCommandTest.java
@@ -0,0 +1,23 @@
+package sg.edu.nus.comp.cs4218.impl.cmd;
+
+import org.junit.Test;
+
+/**
+ * Created by hooitong on 9/2/16.
+ */
+public class SequenceCommandTest {
+    @Test
+    public void testEvaluate() throws Exception {
+
+    }
+
+    @Test
+    public void testParse() throws Exception {
+
+    }
+
+    @Test
+    public void testTerminate() throws Exception {
+
+    }
+}
