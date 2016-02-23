74ef8cad932ebac5a2e280a101a1274255e2f17c
Yeap Hooi Tong <hooitongx@gmail.com>
2016-02-23 12:55:44 +0800

    Added Bc & Cal Test Stubs for EF2

---
+++ test/sg/edu/nus/comp/cs4218/app/BcTest.java
@@ -0,0 +1,96 @@
+package sg.edu.nus.comp.cs4218.app;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Hooi Tong on 2/23/2016.
+ */
+public class BcTest {
+
+    @Test
+    public void testNumber() throws Exception {
+
+    }
+
+    @Test
+    public void testNegate() throws Exception {
+
+    }
+
+    @Test
+    public void testAdd() throws Exception {
+
+    }
+
+    @Test
+    public void testSubtract() throws Exception {
+
+    }
+
+    @Test
+    public void testMultiply() throws Exception {
+
+    }
+
+    @Test
+    public void testDivide() throws Exception {
+
+    }
+
+    @Test
+    public void testPow() throws Exception {
+
+    }
+
+    @Test
+    public void testBracket() throws Exception {
+
+    }
+
+    @Test
+    public void testGreaterThan() throws Exception {
+
+    }
+
+    @Test
+    public void testGreaterThanOrEqual() throws Exception {
+
+    }
+
+    @Test
+    public void testLessThan() throws Exception {
+
+    }
+
+    @Test
+    public void testLessThanOrEqual() throws Exception {
+
+    }
+
+    @Test
+    public void testEqualEqual() throws Exception {
+
+    }
+
+    @Test
+    public void testNotEqual() throws Exception {
+
+    }
+
+    @Test
+    public void testAnd() throws Exception {
+
+    }
+
+    @Test
+    public void testOr() throws Exception {
+
+    }
+
+    @Test
+    public void testNot() throws Exception {
+
+    }
+}
---
+++ test/sg/edu/nus/comp/cs4218/app/CalTest.java
@@ -0,0 +1,41 @@
+package sg.edu.nus.comp.cs4218.app;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Hooi Tong on 2/23/2016.
+ */
+public class CalTest {
+
+    @Test
+    public void testPrintCal() throws Exception {
+
+    }
+
+    @Test
+    public void testPrintCalWithMondayFirst() throws Exception {
+
+    }
+
+    @Test
+    public void testPrintCalForMonthYear() throws Exception {
+
+    }
+
+    @Test
+    public void testPrintCalForYear() throws Exception {
+
+    }
+
+    @Test
+    public void testPrintCalForMonthYearMondayFirst() throws Exception {
+
+    }
+
+    @Test
+    public void testPrintCalForYearMondayFirst() throws Exception {
+
+    }
+}
