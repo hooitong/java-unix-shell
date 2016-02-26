7c0329e465f537c717f11a1eb1fee7812d106735
mohamed aly <mohamed aly>
2016-02-26 17:23:09 +0800

    Fixed DateApplication

--- src/sg/edu/nus/comp/cs4218/impl/app/FmtApplication.java
+++ src/sg/edu/nus/comp/cs4218/impl/app/FmtApplication.java
@@ -14,26 +14,25 @@ import sg.edu.nus.comp.cs4218.Environment;
 import sg.edu.nus.comp.cs4218.exception.FmtException;
 
 /**
- * The fmt command wraps the given text at the specified maximum width without breaking
- * a line in between words.  
+ * The fmt command wraps the given text at the specified maximum width without
+ * breaking a line in between words. 
  * 
  * <p>
  * <b>Command format:</b> <code>fmt [OPTIONS] [FILE]</code>
  * <dl>
  * <dt>OPTIONS</dt>
- * <dd> “​­w 50​” means print the given text where each line has at most 50 characters.     
- * Default value is 80.</dd>
+ * <dd> “​­w 50​” means print the given text where each line has at most 50 
+ * characters.      Default value is 80.</dd>
  * <dt>FILE</dt>
  * <dd>the name of the file(s). If no files are specified, use stdin.</dd>
  * </dl>
  * </p>
  */
-public class FmtApplication implements Application 
-{
+public class FmtApplication implements Application {
 	private static final String CHARSET_UTF_8 = "UTF-8";
 	private static final String NEW_LINE = System.lineSeparator();
 	private static final String WIDTH_FLAG = "-w";
-	
+
 	/**
 	 * Runs the fmt application with the specified arguments.
 	 * 
@@ -51,66 +50,53 @@ public class FmtApplication implements Application
 	 *             If the file(s) specified do not exist or are unreadable.
 	 */
 	@Override
-	public void run(String[] args, InputStream stdin, OutputStream stdout)
-			throws FmtException 
-	{
-		int wrapWidth;
+	public void run(String[] args, InputStream stdin, OutputStream stdout) throws FmtException {
+		int wrapWidth = 80;
 		int filePosition = -1;
-		switch(args.length)
-		{
+		switch (args.length) {
 		case 0:
-			wrapWidth = 80;
+			break;
 		case 1:
-			if(args[0].startsWith(WIDTH_FLAG))
-			{
-				wrapWidth = checkWrapWidth(args[0]);
-			}
-			else
-			{
-				filePosition = 0;
-				wrapWidth = 80;
-			}
+			filePosition = 0;
 			break;
 		case 2:
-			if(args[0].startsWith((WIDTH_FLAG)))
-			{
-				wrapWidth = checkWrapWidth(args[0]);
-				filePosition = 1;
+			if (args[0].equals((WIDTH_FLAG))) {
+				wrapWidth = checkWrapWidth(args[1]);
+			} else {
+				throw new FmtException("Incorrect flag used to denote number of lines to print");
 			}
-			else
-			{
+			break;
+		case 3:
+			if (args[0].equals(WIDTH_FLAG)) {
+				wrapWidth = checkWrapWidth(args[1]);
+				filePosition = 2;
+			} else {
 				throw new FmtException("Incorrect flag used to denote number of lines to print");
 			}
 			break;
 		default:
 			throw new FmtException("Incorrect number of arguments");
 		}
-		
+
 		String inputString = "";
-		if(filePosition>-1)
-		{
+		if (filePosition > -1) {
 			Path currentDir = Paths.get(Environment.currentDirectory);
 			Path filePath = currentDir.resolve(args[filePosition]);
 			boolean isFileReadable = false;
 			isFileReadable = checkIfFileIsReadable(filePath);
-			if (isFileReadable) 
-			{
+			if (isFileReadable) {
 				inputString = readFromFile(filePath);
-			}
-			else
-			{
+			} else {
 				throw new FmtException("File not readable");
 			}
-		}
-		else
-		{
+		} else {
 			inputString = readFromStdin(stdin);
 		}
-		
-		String wrappedString = wrapText(inputString,wrapWidth);
-		writeToStdout(stdout,wrappedString);
+
+		String wrappedString = wrapText(inputString, wrapWidth);
+		writeToStdout(stdout, wrappedString);
 	}
-	
+
 	/**
 	 * Wraps text to specified width without breaking words
 	 * 
@@ -118,93 +104,68 @@ public class FmtApplication implements Application
 	 *            String to wrap
 	 * @param wrapWidth
 	 *            The value of the wrap width
-	 * @return wrappedString 
-	 * 		      Wrapped string
+	 * @return wrappedString Wrapped string
 	 * @throws FmtException
-	 *             If the wrap width is too short (i.e. shorter than the longest word in the string
+	 *             If the wrap width is too short (i.e. shorter than the longest
+	 *             word in the string
 	 */
-	String wrapText(String stringToWrap,int wrapWidth) throws FmtException
-	{
-		if(stringToWrap.isEmpty())
-		{
+	String wrapText(String stringToWrap, int wrapWidth) throws FmtException {
+		if (stringToWrap.isEmpty()) {
 			return "";
 		}
-		stringToWrap = stringToWrap.replaceAll("(\\r|\\n)", " ");
-		String[] strArray = stringToWrap.split(" ");
-		
+		String stringToWrapM = stringToWrap.replaceAll("(\\r|\\n)", " ");
+		String[] strArray = stringToWrapM.split(" ");
 		String wrappedString = "";
-		int i = 0;
-		
+		int count = 0;
 		String tempLine = "";
-		
-		while(i < strArray.length)
-		{
-			String trimmedString = strArray[i].trim();
-			if(tempLine.length()==0)
-			{
-				if(wrapWidth<trimmedString.length())
-				{
+		while (count < strArray.length) {
+			String trimmedString = strArray[count].trim();
+			if (tempLine.length() == 0) {
+				if (wrapWidth < trimmedString.length()) {
 					throw new FmtException("Wrap width too short");
-				}
-				else
-				{
+				} else {
 					tempLine = tempLine.concat(trimmedString);
-					++i;
-					if(i==strArray.length)
-					{
-						wrappedString = wrappedString.concat(NEW_LINE+tempLine);
+					++count;
+					if (count == strArray.length) {
+						wrappedString = wrappedString.concat(NEW_LINE + tempLine);
 					}
 				}
-			}
-			else
-			{
-				if((tempLine.length()+strArray[i].trim().length()+1)<wrapWidth)
-				{			
-					tempLine = tempLine.concat(" "+strArray[i].trim());
-					++i;
-				}
-				else
-				{
-					if(wrappedString=="")
-					{
+			} else {
+				if ((tempLine.length() + strArray[count].trim().length() + 1) < wrapWidth) {
+					tempLine = tempLine.concat(" " + strArray[count].trim());
+					++count;
+				} else {
+					if (wrappedString.isEmpty()) {
 						wrappedString = wrappedString.concat(tempLine);
-					}
-					else
-					{
-						wrappedString = wrappedString.concat(NEW_LINE+tempLine);
+					} else {
+						wrappedString = wrappedString.concat(NEW_LINE + tempLine);
 					}
 					tempLine = "";
 				}
 			}
 		}
-		
 		return wrappedString;
 	}
-	
+
 	/**
 	 * Parses the wrap width from String to int
 	 * 
 	 * @param wrapWidthString
 	 *            The width to wrap received in String
-	 * @return wrapWidth 
-	 * 		 	  The width to wrap received in int
+	 * @return wrapWidth The width to wrap received in int
 	 * @throws FmtException
-	 *            If the wrapWidthString in not an integer or a negative number.
+	 *             If the wrapWidthString in not an integer or a negative
+	 *             number.
 	 */
-	int checkWrapWidth(String wrapWidthString) throws FmtException 
-	{
+	int checkWrapWidth(String wrapWidthString) throws FmtException {
 		int wrapWidth;
-		
-		try 
-		{
-			wrapWidth = Integer.parseInt(wrapWidthString.substring(3, wrapWidthString.length()));
-		} 
-		catch (NumberFormatException nfe) 
-		{
+
+		try {
+			wrapWidth = Integer.parseInt(wrapWidthString);
+		} catch (NumberFormatException nfe) {
 			throw new FmtException("Wrap width not a number");
 		}
-		if(wrapWidth<1)
-		{
+		if (wrapWidth < 1) {
 			throw new FmtException("Wrap width should be at least 1");
 		}
 		return wrapWidth;
@@ -214,138 +175,119 @@ public class FmtApplication implements Application
 	 * Wraps text to specified width without breaking words
 	 * 
 	 * @param stdout
-	 * 			  An OutputStream. Write stringToWrite to this OutputStream.
+	 *            An OutputStream. Write stringToWrite to this OutputStream.
 	 * @param stringToWrite
 	 *            The string to write to stdout
 	 * @throws FmtException
 	 *             If stdout is null or there is an error writing to stdout
 	 */
-	void writeToStdout(OutputStream stdout,String stringToWrite) throws FmtException
-	{
-		if (stdout == null) 
-		{
+	void writeToStdout(OutputStream stdout, String stringToWrite) throws FmtException {
+		if (stdout == null) {
 			throw new FmtException("Null pointer exception - stdout is not defined");
 		}
-		
-		try 
-		{
+
+		try {
 			stdout.write(stringToWrite.getBytes(CHARSET_UTF_8));
-		} 
-		catch (IOException e) 
-		{
+		} catch (IOException e) {
 			throw new FmtException("Error writing to stdout");
 		}
 	}
-	
+
 	/**
-	 * Reads from file 
+	 * Reads from file
 	 *
 	 * @param filePath
 	 *            A Path. Read file from the file path given.
-	 * @return 
-	 * 			  Concatenated string read from file
-	 * @throws FmtException 
-	 * 			  If there is an error reading from the file
+	 * @return Concatenated string read from file
+	 * @throws FmtException
+	 *             If there is an error reading from the file
 	 */
-	String readFromFile(Path filePath) throws FmtException
-	{
+	String readFromFile(Path filePath) throws FmtException {
 		String concatString = "";
-		
-		try 
-		{
+
+		try {
 			FileInputStream fileInStream = new FileInputStream(filePath.toString());
 			BufferedReader buffReader = new BufferedReader(new InputStreamReader(fileInStream));
-			
+
 			String input = "";
-			
-			while ((input = buffReader.readLine()) != null) 
-			{
+
+			while ((input = buffReader.readLine()) != null) {
 				concatString = concatString.concat(input);
 			}
-			
+
 			buffReader.close();
-		} 
-		catch (IOException e) 
-		{
+		} catch (IOException e) {
 			throw new FmtException("Error reading from file");
 		}
-		
+
 		return concatString;
 	}
-	
+
 	/**
-	 * Reads from stdin 
+	 * Reads from stdin
 	 *
 	 * @param stdin
 	 *            An InputStream. Read input from this InputStream.
-	 * @return 
-	 * 			  Concatenated string read from stdin
-	 * @throws FmtException 
-	 * 			  If stdin is null or there is an error reading from stdin
+	 * @return Concatenated string read from stdin
+	 * @throws FmtException
+	 *             If stdin is null or there is an error reading from stdin
 	 */
-	String readFromStdin(InputStream stdin) throws FmtException
-	{
+	String readFromStdin(InputStream stdin) throws FmtException {
 		String concatString = "";
-		
-		try
-		{
-			if (stdin == null) 
-			{
+
+		try {
+			if (stdin == null) {
 				throw new FmtException("Null pointer exception - stdin is not defined");
 			}
-			
+
 			BufferedReader buffReader = new BufferedReader(new InputStreamReader(stdin));
-	
+
 			String input = "";
-			
-			while ((input = buffReader.readLine()) != null) 
-			{
+
+			while ((input = buffReader.readLine()) != null) {
 				concatString = concatString.concat(input);
 			}
-			
+
 			buffReader.close();
-		}
-		catch (Exception e) 
-		{
+		} catch (Exception e) {
 			throw new FmtException("Error reading from stdin");
 		}
-		
+
 		return concatString;
 	}
-	
+
 	/**
 	 * Checks if a file is readable
 	 * 
 	 * @param filePath
 	 *            The path to the file
-	 * @return 
-	 * 			  True if the file is readable
+	 * @return True if the file is readable
 	 * @throws FmtException
-	 *            If the file is not readable
+	 *             If the file is not readable
 	 */
-	boolean checkIfFileIsReadable(Path filePath) throws FmtException 
-	{
-		if (Files.isDirectory(filePath)) 
-		{
+	boolean checkIfFileIsReadable(Path filePath) throws FmtException {
+		if (Files.isDirectory(filePath)) {
 			throw new FmtException("This is a directory");
 		}
-		if (Files.isReadable(filePath)) 
-		{
+		if (Files.isReadable(filePath)) {
 			return true;
-		} 
-		else 
-		{
+		} else {
 			throw new FmtException("Could not read file");
 		}
 	}
 }
 
-//References
+// References
 
-//http://stackoverflow.com/questions/6684665/java-byte-array-to-string-to-byte-array
-//Question by : http://stackoverflow.com/users/843387/0909em
-//Answer by : http://stackoverflow.com/users/320700/yanick-rochon
+// http://stackoverflow.com/questions/6684665/java-byte-array-to-string-to-byte-array
+// Question by : http://stackoverflow.com/users/843387/0909em
+// Answer by : http://stackoverflow.com/users/320700/yanick-rochon
 
-//http://stackoverflow.com/questions/593671/remove-end-of-line-characters-from-java-string
+// http://stackoverflow.com/questions/593671/remove-end-of-line-characters-from-java-string
 
-//http://www.mkyong.com/java/how-to-get-the-standard-input-in-java/
+// http://www.mkyong.com/java/how-to-get-the-standard-input-in-java/
+
+// Check emtpy string
+// http://stackoverflow.com/questions/3598770/java-check-whether-a-string-is-not-null-and-not-empty
+// Question by : http://stackoverflow.com/users/405398/xyz
+// Answer by : http://stackoverflow.com/users/422597/colin-hebert
--- src/sg/edu/nus/comp/cs4218/impl/app/TailApplication.java
+++ src/sg/edu/nus/comp/cs4218/impl/app/TailApplication.java
@@ -36,7 +36,7 @@ public class TailApplication implements Application {
 	private static final String CHARSET_UTF_8 = "UTF-8";
 	private static final String NEW_LINE = System.lineSeparator();
 	private static final String NUMLINES_FLAG = "-n";
-	
+
 	/**
 	 * Runs the tail application with the specified arguments.
 	 * 
@@ -61,64 +61,53 @@ public class TailApplication implements Application {
 	 *             criteria.
 	 */
 	@Override
-	public void run(String[] args, InputStream stdin, OutputStream stdout)
-			throws TailException 
-	{
-		int numLinesToRead;
+	public void run(String[] args, InputStream stdin, OutputStream stdout) throws TailException {
+		int numLinesToRead = 15;
 		int filePosition = -1;
-		switch(args.length)
-		{
-		case 0:
-			numLinesToRead = 15;
-		case 1:
-			if(args[0].startsWith(NUMLINES_FLAG))
-			{
-				numLinesToRead = checkNumberOfLinesInput(args[0]);
-			}
-			else
-			{
+		if (args != null) {
+			switch (args.length) {
+			case 0:
+				break;
+			case 1:
 				filePosition = 0;
-				numLinesToRead = 15;
+				break;
+			case 2:
+				if (args[0].equals((NUMLINES_FLAG))) {
+					numLinesToRead = checkNumberOfLinesInput(args[1]);
+				} else {
+					throw new TailException("Incorrect flag used to denote number of lines to print");
+				}
+				break;
+			case 3:
+				if (args[0].equals(NUMLINES_FLAG)) {
+					numLinesToRead = checkNumberOfLinesInput(args[1]);
+					filePosition = 2;
+				} else {
+					throw new TailException("Incorrect flag used to denote number of lines to print");
+				}
+				break;
+			default:
+				throw new TailException("Incorrect number of arguments");
 			}
-			break;
-		case 2:
-			if(args[0].startsWith((NUMLINES_FLAG)))
-			{
-				numLinesToRead = checkNumberOfLinesInput(args[0]);
-				filePosition = 1;
-			}
-			else
-			{
-				throw new TailException("Incorrect flag used to denote number of lines to print");
-			}
-			break;
-		default:
-			throw new TailException("Incorrect number of arguments");
 		}
-		
+
 		Stack<String> inputString = null;
-		if(filePosition>-1)
-		{
+		if (filePosition > -1) {
 			Path currentDir = Paths.get(Environment.currentDirectory);
 			Path filePath = currentDir.resolve(args[filePosition]);
 			boolean isFileReadable = false;
 			isFileReadable = checkIfFileIsReadable(filePath);
-			if (isFileReadable) 
-			{
+			if (isFileReadable) {
 				inputString = readFromFile(filePath);
-			}
-			else
-			{
+			} else {
 				throw new TailException("File not readable");
 			}
-		}
-		else
-		{
+		} else {
 			inputString = readFromStdin(stdin);
 		}
-		
-		LinkedList<String> wrappedString = extractTail(inputString,numLinesToRead);
-		writeToStdout(stdout,wrappedString);
+
+		LinkedList<String> wrappedString = extractTail(inputString, numLinesToRead);
+		writeToStdout(stdout, wrappedString);
 	}
 
 	/**
@@ -126,25 +115,19 @@ public class TailApplication implements Application {
 	 * 
 	 * @param numLinesString
 	 *            The number of lines received in String
-	 * @return numLines 
-	 * 		The number of lines received in int
+	 * @return numLines The number of lines received in int
 	 * @throws TailException
 	 *             If the numLinesString in not an Integer or a negative number.
 	 */
-	int checkNumberOfLinesInput(String numLinesString) throws TailException 
-	{
+	int checkNumberOfLinesInput(String numLinesString) throws TailException {
 		int numLines;
 
-		try 
-		{
-			numLines = Integer.parseInt(numLinesString.substring(3, numLinesString.length()));
-		} 
-		catch (NumberFormatException nfe) 
-		{
+		try {
+			numLines = Integer.parseInt(numLinesString);
+		} catch (NumberFormatException nfe) {
 			throw new TailException("Number of lines not a number");
 		}
-		if(numLines<0)
-		{
+		if (numLines < 0) {
 			throw new TailException("Number of lines should be at least 0");
 		}
 		return numLines;
@@ -157,164 +140,131 @@ public class TailApplication implements Application {
 	 *            String to wrap
 	 * @param wrapWidth
 	 *            The value of the wrap width
-	 * @return wrappedString 
-	 * 		      Wrapped string
+	 * @return wrappedString Wrapped string
 	 * @throws TailException
-	 *             If the wrap width is too short (i.e. shorter than the longest word in the string
+	 *             If the wrap width is too short (i.e. shorter than the longest
+	 *             word in the string
 	 */
-	LinkedList<String> extractTail(Stack<String> textToExtractFrom,int numLines) throws TailException
-	{
+	LinkedList<String> extractTail(Stack<String> textToExtractFrom, int numLines) throws TailException {
 		LinkedList<String> extractedText = new LinkedList<String>();
-		if(textToExtractFrom.isEmpty()||numLines==0)
-		{
+		if (textToExtractFrom.isEmpty() || numLines == 0) {
 			return extractedText;
 		}
 		int count = 0;
-		while(count < numLines)
-		{
-			if(!textToExtractFrom.isEmpty())
-			{
+		while (count < numLines) {
+			if (textToExtractFrom.isEmpty()) {
+				break;
+			} else {
 				extractedText.addFirst(textToExtractFrom.pop());
 				++count;
 			}
-			else
-			{
-				break;
-			}
 		}
-		
+
 		return extractedText;
 	}
-	
-	
+
 	/**
 	 * Checks if a file is readable.
+	 * 
 	 * @param filePath
-	 * 		The path to the file
-	 * @return
-	 * 		True if the file is readable.
+	 *            The path to the file
+	 * @return True if the file is readable.
 	 * @throws TailException
-	 * 		If the file is not readable
+	 *             If the file is not readable
 	 */
-	boolean checkIfFileIsReadable(Path filePath) throws TailException 
-	{	
-		if (Files.exists(filePath) && Files.isReadable(filePath)) 
-		{
+	boolean checkIfFileIsReadable(Path filePath) throws TailException {
+		if (Files.exists(filePath) && Files.isReadable(filePath)) {
 			return true;
-		} 
-		else 
-		{
+		} else {
 			throw new TailException("Could not read file");
 		}
 	}
-	
+
 	/**
 	 * Writes to specified OutputStream
 	 * 
 	 * @param stdout
-	 * 			  An OutputStream. Write stringToWrite to this OutputStream.
+	 *            An OutputStream. Write stringToWrite to this OutputStream.
 	 * @param linesToWrite
 	 *            The lines to write to stdout
 	 * @throws TailException
 	 *             If stdout is null or there is an error writing to stdout
 	 */
-	void writeToStdout(OutputStream stdout,LinkedList<String> linesToWrite) throws TailException
-	{
-		if (stdout == null) 
-		{
+	void writeToStdout(OutputStream stdout, LinkedList<String> linesToWrite) throws TailException {
+		if (stdout == null) {
 			throw new TailException("Null pointer exception - stdout is not defined");
 		}
-		
-		try 
-		{
-			while(!linesToWrite.isEmpty())
-			{
+
+		try {
+			while (!linesToWrite.isEmpty()) {
 				stdout.write(linesToWrite.removeFirst().getBytes(CHARSET_UTF_8));
-				if(!linesToWrite.isEmpty())
-				{
+				if (!linesToWrite.isEmpty()) {
 					stdout.write(NEW_LINE.getBytes(CHARSET_UTF_8));
 				}
 			}
-		} 
-		catch (IOException e) 
-		{
+		} catch (IOException e) {
 			throw new TailException("Error writing to stdout");
 		}
 	}
-	
+
 	/**
-	 * Reads from file 
+	 * Reads from file
 	 *
 	 * @param filePath
 	 *            A Path. Read file from the file path given.
-	 * @return textToExtract
-	 * 			  Stack of read lines
-	 * @throws TailException 
-	 * 			  If there is an error reading from the file
+	 * @return textToExtract Stack of read lines
+	 * @throws TailException
+	 *             If there is an error reading from the file
 	 */
-	Stack<String> readFromFile(Path filePath) throws TailException
-	{
+	Stack<String> readFromFile(Path filePath) throws TailException {
 		Stack<String> textToExtract = new Stack<String>();
-		
-		try 
-		{
+
+		try {
 			FileInputStream fileInStream = new FileInputStream(filePath.toString());
 			BufferedReader buffReader = new BufferedReader(new InputStreamReader(fileInStream));
-			
+
 			String input = "";
-			
-			while ((input = buffReader.readLine()) != null) 
-			{
+
+			while ((input = buffReader.readLine()) != null) {
 				textToExtract.push(input);
 			}
-			
+
 			buffReader.close();
-		} 
-		catch (IOException e) 
-		{
+		} catch (IOException e) {
 			throw new TailException("Error reading from file");
 		}
 		return textToExtract;
 	}
-	
+
 	/**
-	 * Reads from stdin 
+	 * Reads from stdin
 	 *
 	 * @param stdin
 	 *            An InputStream. Read input from this InputStream.
-	 * @return textToExtract
-	 * 			  Stack of read lines
-	 * @throws TailException 
-	 * 			  If stdin is null or there is an error reading from stdin
+	 * @return textToExtract Stack of read lines
+	 * @throws TailException
+	 *             If stdin is null or there is an error reading from stdin
 	 */
-	Stack<String> readFromStdin(InputStream stdin) throws TailException
-	{
+	Stack<String> readFromStdin(InputStream stdin) throws TailException {
 		Stack<String> textToExtract = new Stack<String>();
-		
-		try
-		{
-			if (stdin == null) 
-			{
-				throw new TailException("Null pointer exception - stdin is not defined");
-			}
-			
+		if (stdin == null) {
+			throw new TailException("Null pointer exception - stdin is not defined");
+		}
+		try {
 			BufferedReader buffReader = new BufferedReader(new InputStreamReader(stdin));
-	
+
 			String input = "";
-			
-			while ((input = buffReader.readLine()) != null) 
-			{
+
+			while ((input = buffReader.readLine()) != null) {
 				textToExtract.push(input);
 			}
-			
+
 			buffReader.close();
-		}
-		catch (Exception e) 
-		{
+		} catch (Exception e) {
 			throw new TailException("Error reading from stdin");
 		}
-		
+
 		return textToExtract;
 	}
-	
+
 }
