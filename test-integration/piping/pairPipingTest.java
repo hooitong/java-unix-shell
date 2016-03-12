fb3f86cb97b25ba98c26fbc5ed51c7038e5f27c7
Yeap Hooi Tong <hooitongx@gmail.com>
2016-03-12 15:31:19 +0800

    Added manually generated test stubs for pair-piping integration tests

---
+++ test-integration/piping/pairPipingTest.java
@@ -0,0 +1,121 @@
+package piping;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class PairPipingTest {
+    @Before
+    public void setUp() {
+
+    }
+
+    @Test
+    public void pipeCatFmt() {
+
+    }
+
+    @Test
+    public void pipeCatEcho() {
+
+    }
+
+    @Test
+    public void pipeEchoSort() {
+
+    }
+
+    @Test
+    public void pipeEchoCal() {
+
+    }
+
+    @Test
+    public void pipeHeadBc() {
+
+    }
+
+    @Test
+    public void pipeHeadCat() {
+
+    }
+
+    @Test
+    public void pipeTailCat() {
+
+    }
+
+    @Test
+    public void pipeTailCal() {
+
+    }
+
+    @Test
+    public void pipeDateFmt() {
+
+    }
+
+    @Test
+    public void pipeDateComm() {
+
+    }
+
+    @Test
+    public void pipeFmtTail() {
+
+    }
+
+    @Test
+    public void pipeFmtBc() {
+
+    }
+
+    @Test
+    public void pipeSortComm() {
+
+    }
+
+    @Test
+    public void pipeSortEcho() {
+
+    }
+
+    @Test
+    public void pipeCommHead() {
+
+    }
+
+    @Test
+    public void pipeCommNegative() {
+
+    }
+
+    @Test
+    public void pipeBcSort() {
+
+    }
+
+    @Test
+    public void pipeBcDate() {
+
+    }
+
+    @Test
+    public void pipeCalHead() {
+
+    }
+
+    @Test
+    public void pipeCalNegative() {
+
+    }
+
+    @Test
+    public void pipeNegativeTail() {
+
+    }
+
+    @Test
+    public void pipeNegativeDate() {
+        
+    }
+}
