bdb624e5ab770f91ffa3ca7d024b23348eff56e9
Yeap Hooi Tong <hooitongx@gmail.com>
2016-03-26 13:28:14 +0800

    Added one more bug and missing files

--- hackathon-submission/test-hackathon/BcApplicationHack.java
+++ hackathon-submission/test-hackathon/BcApplicationHack.java
@@ -49,4 +49,23 @@ public class BcApplicationHack {
         bcApp.run(params, inStream, outStream);
         assertEquals("10", outStream.toString());
     }
+
+    /**
+     * Fails to return 1. In standard logical AND operator convention, only 0 stands for false while any another value
+     * except 0 represents true. Similar for OR.
+     *
+     * This bug despite not mentioned in the specification should be clarified by the comments of the team on how
+     * the operators work. It is stated in the announcement that "You can only defend formally if you have evidence,
+     * e.g., by saying so in the documentation or with a test case that exercises this supposedly buggy functionality
+     * but passes.". As such we use the standard convention used by multiple languages and official UNIX Bc specification
+     * to evaluate your operators. If we do not do so then your operators correctness cannot be tested which questions
+     * the purpose of testing Bc.
+     *
+     */
+    @Test
+    public void testAndNegativeIntegers() throws Exception {
+        String[] params = { "-1 && -1" };
+        bcApp.run(params, inStream, outStream);
+        assertEquals("1", outStream.toString());
+    }
 }
